df4857d796e6d8fe80d5d1319d89afb049ff2960
gabburgio <gabbburgio@gmail.com>
2023-07-18 15:21:20 +0200

    a

--- scripts/mnr/albedo.py
+++ scripts/mnr/albedo.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+
 def writealbedo(res_path, path, group_number):
     ALB_IN_CURR = []
     ALB_OUT_CURR = []
     ALB_TOT_ALB = []
     lines = []
-    surface_number = 5
+    surface_number = 6
     
     with open(res_path, 'r') as r:
         lines = r.readlines()
@@ -24,51 +25,24 @@ def writealbedo(res_path, path, group_number):
             for string in intermediate: 
                 ALB_TOT_ALB.append(string.split(" ")[0])
 
-
-    #compute albedo matrix for NT surface
-
     ALB_IN_CURR = list(map(float, ALB_IN_CURR))
     ALB_OUT_CURR = list(map(float, ALB_OUT_CURR))
     ALB_TOT_ALB = list(map(float, ALB_TOT_ALB))
 
-    denom = np.zeros(group_number)
-    numer = np.zeros(group_number*group_number)
-    totalb = np.zeros(group_number)
-    check = []
+    shape = (surface_number, group_number, surface_number, group_number)
 
+    tensor_aoc = np.empty(shape)
 
-    #check that surface 6 is the top (nothing comes back from it)
-    '''
-    for j in range(6):
-        check.append(sum(ALB_OUT_CURR[24*j:24*(j+1)]))
-    print(check)'''
-
-    #check that caclulated albedo for the total surface corresponds to the one given by serpent by switching to 6
-    #surface_number = 6
+    for i in range(len(ALB_OUT_CURR)):
+        tensor_aoc[np.unravel_index(i, shape)] = ALB_OUT_CURR[i]
     
-
-    for i in range(group_number*surface_number):
-        if (i%2 == 0):
-            denom[0] += ALB_IN_CURR[i]
-        if (i%2 == 1):
-            denom[1] += ALB_IN_CURR[i]
-
-    for i in range(group_number**2 * 6**2):
-        if(i%group_number==0):
-            if((i//(group_number*6))%group_number == 0):
-                numer[0]+= ALB_OUT_CURR[i]
-            if((i//(group_number*6))%group_number == 1):
-                numer[1]+= ALB_OUT_CURR[i]
-        if(i%group_number==1):
-            if((i//(group_number*6))%group_number == 0):
-                numer[2]+= ALB_OUT_CURR[i]
-            if((i//(group_number*6))%group_number == 1):
-                numer[3]+= ALB_OUT_CURR[i]
-                
-    for i in range(len(ALB_TOT_ALB)):
-        numer[i] = numer[i]/denom[i%2]
-    print(ALB_TOT_ALB)
-    print(numer)
+    print(tensor_aoc)
 
     with open(path, 'a') as p:
         p.write("\n[BCs]\n" + "[./albedo]\n" + "\ttype = ArrayAlbedoBC \n" )
+
+
+res_path = 'MNR_63V.inp_res.m'
+output_path = "mnr_sphdf.i"
+
+writealbedo(res_path, output_path, 2)
--- scripts/mnr/tensorize.py
+++ scripts/mnr/tensorize.py
@@ -1,53 +1,43 @@
 import numpy as np
 
-def array_index(tensor_indexes, multipliers):
+def array_index(tensor_indexes, tensor_dimensions):
+    multipliers = [1]
+    for i in range(1,len(tensor_dimensions)):
+        multipliers.append(multipliers[i-1]*tensor_dimensions[i-1])
+    print(multipliers)
+
     array_index = 0
     for i, index in enumerate(tensor_indexes):
         array_index += index*multipliers[i]
     return array_index
 
 
-
-def multipliers(tensor_dimensions):
-    multipliers = [1]
-    for i in range(1,len(tensor_dimensions)):
-        multipliers.append(multipliers[i-1]*tensor_dimensions[i-1])
-    return multipliers
-
-
 def filler(tensor_dimension, previous_index, previous_point, tensor_index_array):
-    
-    tot_entries = 1
-    seen_entries = 1
-
-    for dimension in tensor_dimension:
-        tot_entries *= dimension
-    
-    for index in previous_index:
-        seen_entries *= index
-
-    if(tot_entries == seen_entries):
-        print("end reached")
-        return tensor_index_array
     
     if (previous_index[previous_point] < tensor_dimension[previous_point]):
         previous_index[previous_point]+= 1
-        print("adding index:" + str(previous_index))
-        print("former tensor array = " + str(tensor_index_array)) 
-
-        tensor_index_array.append(previous_index)
-        print("new tensor array = " + str(tensor_index_array)) 
-        filler(tensor_dimension, previous_index, previous_point, tensor_index_array)
+        tensor_index_array.append(previous_index[:])
+        if(tensor_dimension == previous_index):
+            return tensor_index_array
+        return filler(tensor_dimension, previous_index, previous_point, tensor_index_array)
 
     if (previous_index[previous_point] == tensor_dimension[previous_point]):
         previous_point +=1
-        print("pass")
-        filler(tensor_dimension, previous_index, previous_point, tensor_index_array)
+        return filler(tensor_dimension, previous_index, previous_point, tensor_index_array)
 
-    
-#def as_tensor(array, tensor_dimensions):
 
-dims = [2,4,3,6]
+def as_tensor(array, tensor_dimension):
+    zero_index = [0]*len(tensor_dimension)
+    starting_list = [zero_index[:]]
+    indexes = filler(tensor_dimension, zero_index, 0, starting_list )
+    print(indexes)
+    tensor = np.zeros(tensor_dimension)
+    for index in indexes:
+        tensor[index] = array[array_index(index, tensor_dimension)]
+    return tensor
 
-str(filler(dims, [0,0,0,0], 0, [] ))
 
+array = [1,2,3,4,5,6,7,8]
+dims = [2,4, 3,6]
+
+print(as_tensor(array, dims))
