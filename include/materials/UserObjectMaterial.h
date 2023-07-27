b1eee2ed686e9b32cf2d1500c65625a148d480fb
gabburgio <gabbburgio@gmail.com>
2023-07-27 15:58:55 +0200

    a

---
+++ include/materials/UserObjectMaterial.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Material.h"
+
+class ElementIntegralVariableUserObject;
+class ElementIntegralUserObject;
+
+class UserObjectMaterial : public Material
+{
+public:
+    
+    UserObjectMaterial(const InputParameters & parameters);
+
+    static InputParameters validParams();
+    
+protected:
+    virtual void computeQpProperties() override;
+    
+    const ElementIntegralVariableUserObject & _uo;
+    const Real & _v_cross_section;
+    const Real & _ref_int;
+    
+    MaterialProperty<Real> & _cross_section;
+    
+};
---
+++ src/materials/UserObjectMaterial.C
@@ -0,0 +1,33 @@
+#include "UserObjectMaterial.h"
+#include "ElementIntegralVariableUserObject.h"
+#include "ElementIntegralUserObject.h"
+
+registerMooseObject("pertApp",UserObjectMaterial);
+
+InputParameters
+UserObjectMaterial::validParams()
+{
+    InputParameters params = Material::validParams();
+    params.addRequiredParam<UserObjectName>("user_object", "___");    
+    params.addRequiredParam<Real>("cross_section","___");        
+    params.addRequiredParam<Real>("ref_int","___");
+    return params;
+}
+
+
+
+UserObjectMaterial::UserObjectMaterial(const InputParameters & parameters) :
+    Material(parameters),
+
+    _uo(getUserObject<ElementIntegralVariableUserObject>("user_object")),
+    _v_cross_section(     getParam<Real>("cross_section")),
+    _ref_int(     getParam<Real>("ref_int")),
+    _cross_section(       declareProperty<Real>("cross_section"))
+{
+}
+
+
+void UserObjectMaterial::computeQpProperties()
+{
+    _cross_section[_qp]       = (_ref_int/_uo.ElementIntegralUserObject::getValue())*_v_cross_section;
+}
